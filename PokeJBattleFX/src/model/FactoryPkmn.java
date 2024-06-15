6ca7ab781e9d9000e6b5150339a048d49ba93e22
PatatineFritte0 <simocomignani@gmail.com>
2024-06-15 12:48:00 +0200

    fix factory 2

---
+++ PokeJBattleFX/src/model/FactoryPkmn.java
@@ -0,0 +1,52 @@
+package model;
+import model.pokemon.*;
+
+public class FactoryPkmn {
+
+	private Pokemon pkmn; 
+	
+	public Pokemon crea(String scelta, int... lvl) {
+		switch(scelta) {
+			case "bulbasaur":
+				this.pkmn = new Bulbasaur();
+				break;
+			case "ivysaur":
+				this.pkmn = new Ivysaur();
+				break;
+			case "venosaur":
+				this.pkmn = new Venosaur();
+				break;
+			case "squirtle":
+				this.pkmn = new Squirtle();
+				break;
+			case "wortortle":
+				this.pkmn = new Wortortle();
+				break;
+			case "blastoise":
+				this.pkmn = new Blastoise();
+				break;
+			case "charmander":
+				this.pkmn = new Charmander();
+				break;
+			case "charmeleon":
+				this.pkmn = new Charmeleon();
+				break;
+			case "charizard":
+				this.pkmn = new Charizard();
+				break;
+			case "missingNo":
+				this.pkmn = new MissingNo();
+				break;
+			default:
+				this.pkmn = null;
+		}
+		
+		int l = 0;
+        for (int i : lvl) { l += i; }
+        l = (l>1)? l-1 : 0;
+        
+        if(l != 0) {this.pkmn.goToLvl(l);}
+		
+		return this.pkmn;
+	}
+}
